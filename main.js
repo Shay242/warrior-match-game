cb4fa8bf60e453d05e54d47c5a934a28635f5cd6
Shay Perryman <shayperryman@Shays-MacBook-Pro.local>
2022-03-19 21:55:11 -0400

    javascript code

--- main.js
+++ main.js
@@ -8,10 +8,17 @@ const buildHtml = () => {
   const cardContainer = document.createElement("div");
   const frontCard = document.createElement("div");
   const img1 = document.createElement("img");
+  const backCard = document.createElement("div");
 
-  firstRow.className.add("first-row");
-  cardContainer.className.add("card-container");
-  frontCard.className.add("front-card");
+
+//   for (let i = 0; i < 3; i++ ) {
+//       const div = document.createElement("div")
+//      cardContainer.appendChild(`div${i}`); 
+//   }
+
+  firstRow.classList.add("first-row");
+  cardContainer.classList.add("card-container");
+  frontCard.classList.add("front-card");
 
   img1.src =
     "https://image.shutterstock.com/image-vector/trendy-lettering-queen-gold-crown-260nw-1748371328.jpg";
@@ -19,4 +26,79 @@ const buildHtml = () => {
   cardContainer.appendChild(frontCard);
   firstRow.appendChild(cardContainer);
   build.appendChild(firstRow);
+};
+
+const buildHtml = () => {
+  const build2 = document.getElementById("build2");
+  const secondRow = document.createElement("div");
+  const cardContainer = document.createElement("div");
+  const frontCard = document.createElement("div");
+  const img1 = document.createElement("img");
+  const backCard = document.createElement("div");
+
+  //   for (let i = 0; i < 3; i++ ) {
+  //       const div = document.createElement("div")
+  //      cardContainer.appendChild(`div${i}`);
+  //   }
+
+  secondRow.classList.add("second-row");
+  cardContainer.classList.add("card-container");
+  frontCard.classList.add("front-card");
+
+  img1.src =
+    "https://image.shutterstock.com/image-vector/trendy-lettering-queen-gold-crown-260nw-1748371328.jpg";
+
+  cardContainer.appendChild(frontCard);
+  secondRow.appendChild(cardContainer);
+  build.appendChild(secondRow);
+};
+
+const buildHtml = () => {
+  const build3 = document.getElementById("build3");
+  const thirdRow = document.createElement("div");
+  const cardContainer = document.createElement("div");
+  const frontCard = document.createElement("div");
+  const img1 = document.createElement("img");
+  const backCard = document.createElement("div");
+
+  //   for (let i = 0; i < 3; i++ ) {
+  //       const div = document.createElement("div")
+  //      cardContainer.appendChild(`div${i}`);
+  //   }
+
+  thirdRow.classList.add("third-row");
+  cardContainer.classList.add("card-container");
+  frontCard.classList.add("front-card");
+
+  img1.src =
+    "https://image.shutterstock.com/image-vector/trendy-lettering-queen-gold-crown-260nw-1748371328.jpg";
+
+  cardContainer.appendChild(frontCard);
+  thirdRow.appendChild(cardContainer);
+  build.appendChild(thirdRow);
+};
+
+const buildHtml = () => {
+  const build4 = document.getElementById("build4");
+  const fourthRow = document.createElement("div");
+  const cardContainer = document.createElement("div");
+  const frontCard = document.createElement("div");
+  const img1 = document.createElement("img");
+  const backCard = document.createElement("div");
+
+  //   for (let i = 0; i < 3; i++ ) {
+  //       const div = document.createElement("div")
+  //      cardContainer.appendChild(`div${i}`);
+  //   }
+
+  fourthRow.classList.add("fourth-row");
+  cardContainer.classList.add("card-container");
+  frontCard.classList.add("front-card");
+
+  img1.src =
+    "https://image.shutterstock.com/image-vector/trendy-lettering-queen-gold-crown-260nw-1748371328.jpg";
+
+  cardContainer.appendChild(frontCard);
+  fourthRow.appendChild(cardContainer);
+  build.appendChild(fourthRow);
 };
