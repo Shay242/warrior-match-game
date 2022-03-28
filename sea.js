afed9e2218a9d5a138df79308a7b0ce774ae4101
JohnsonSJ22 <shanikaajohnson@gmail.com>
2022-03-28 18:42:10 -0400

    finished coding out sea html and sea javascript

--- sea.js
+++ sea.js
@@ -7,6 +7,10 @@ const answerButtonsElement = document.getElementById('answer-buttons')
 let shuffledQuestions, currentQuestionIndex
 
 startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', () => {
+    currentQuestionIndex++
+    setNextQuestion()
+})
 
 function startGame() {
   startButton.classList.add('hide')
@@ -37,7 +41,7 @@ function showQuestion(question) {
   questionElement.innerText = question.question
   question.answers.forEach(answer => {
     const button = document.createElement('button')
-    button.innerText = answer. text
+    button.innerText = answer.text
     button.classList.add('btn')
     if (answer.correct) {
       button.dataset.correct =answer.correct
@@ -48,6 +52,7 @@ function showQuestion(question) {
 }
 
 function resetState() {
+    clearStatusClass(document.body)
     nextButton.classList.add('hide')
     while (answerButtonsElement.firstChild) {
         answerButtonsElement.removeChild
@@ -56,16 +61,68 @@ function resetState() {
 }
 
 function selectAnswer(e) {
+    const selectedButton = e.target
+    const correct = selectedButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerButtonsElement.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    })
+    if (shuffledQuestions.length > currentQuestionIndex + 1) {
+        nextButton.classList.remove('hide')
+    } else {
+        startButton.innerText = 'Restart'
+        startButton.classList.remove('hide')
+    }
+}
 
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add('correct')
+    } else {
+        element.classList.add('wrong')
+    }
+}
+
+function clearStatusClass(element) {
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
 }
 const questions = [
   {
     question: 'What checks only if the value is the same?',
     answers: [
-      { text: 'one equal', correct: false },
-      { text: 'three equals', correct: false },
-      { text: 'two equals', correct: true },
-      { text: 'dash and equal', correct: false },
+      { text: '=', correct: false },
+      { text: '===', correct: false },
+      { text: '==', correct: true },
+      { text: '-=', correct: false },
+    ],
+  },
+  {
+    question: 'What are the types of variables?',
+    answers: [
+      { text: 'string, value, number, name', correct: false },
+      { text: 'keyword, objects, value, array', correct: false },
+      { text: 'number, string, objects, vlaue', correct: false },
+      { text: 'string, number, array, object', correct: true },
+    ],
+  },
+  {
+    question: 'How to create a function?',
+    answers: [
+      { text: 'name, value, keyword', correct: false },
+      { text: 'keyword, value, name, expression', correct: false },
+      { text: 'keyword, name, value', correct: true },
+      { text: 'expression, value, name', correct: false },
+    ],
+  },
+  {
+    question: 'Adding what to a child overides the parent?',
+    answers: [
+      { text: 'display flex', correct: false },
+      { text: 'absolute', correct: true },
+      { text: 'boolen', correct: false },
+      { text: 'function', correct: false },
     ],
   },
 ];
